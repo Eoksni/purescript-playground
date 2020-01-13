92c8a8d66712a2afda8e9940b43ea6568004d099
Dmitry Mazurok <mazurok@outlook.com>
2020-01-13 21:55:43 +0000

    basic stuff

--- lens/spago.dhall
+++ lens/spago.dhall
@@ -3,7 +3,8 @@ Welcome to a Spago project!
 You can edit this file as you like.
 -}
 { name = "my-project"
-, dependencies = [ "console", "effect", "psci-support" ]
+, dependencies =
+    [ "console", "effect", "profunctor-lenses", "psci-support", "strings" ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- lens/src/Main.purs
+++ lens/src/Main.purs
@@ -1,10 +1,21 @@
 module Main where
 
 import Prelude
-
+import Data.Lens (Lens, lens, over, set, view)
+import Data.String as String
+import Data.Tuple (Tuple(..), fst)
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Console (log, logShow)
+
+_first :: forall a b ignored. Lens (Tuple a ignored) (Tuple b ignored) a b
+_first = lens getter setter
+  where
+  getter = fst
+
+  setter (Tuple _ kept) new = Tuple new kept
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  log $ view _first $ Tuple "one" 1
+  logShow $ set _first "hello" $ Tuple 1 1
+  logShow $ over _first String.length $ Tuple "hi" 123
